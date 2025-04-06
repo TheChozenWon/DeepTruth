fbe7ef20e9db49fc2a6d0ec27c2b48d31528bd73
Jay D Doshi <workingjay2010@gmail.com>
2025-04-06 01:16:12 -0400

    Update manifest and popup.js to include new API endpoint and permissions

--- Chrome Extension/popup.js
+++ Chrome Extension/popup.js
@@ -117,7 +117,7 @@ async function verifyArticle(title) {
     }
 
     try {
-        const response = await fetch("http://127.0.0.1:8000/api/verify-claim/", {
+        const response = await fetch("https://api.deeptruth.us/api/verify-claim/", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
